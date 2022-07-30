0b9186d1667ed34092bed911e8ee9f7560c999b4
JUBIN <jubin3928@gmail.com>
2022-07-30 16:13:06 +0900

    jsday1최종

---
+++ jsday1/test.js
@@ -0,0 +1,19 @@
+//주말에 node.js 설치
+//vs code에서 스크립트실행 허용이 안된 경우 powershell 에서 설정 변경
+console.log('사과바나나기러기스위스토마토역삼역')
+
+let arr1=[11,22,44,99,55]
+        console.log(arr1)
+        console.log(typeof arr1)
+        console.log(arr1[3])
+        arr1[2]=77
+        console.log(arr1.sort())
+        console.log(arr1.reverse())
+        arr1[5]=44
+        console.log(arr1)
+        arr1=[1,5,6,7]; //let 변수 이므로 객체 자체 변경가능
+
+        const arr2=['apple','mango','kiwi','banana','딸기','사과']
+        console.log(arr2)
+        arr2[7]='배'        //기존객체 변경 가능
+        console.log(arr2)
